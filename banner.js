3bf4ce5a32f20f3b8a690c587dc860818e6b822b
Boyoke Encok <mysqlproject98@gmail.com>
2025-02-09 11:57:06 +0700

    Add files via upload

---
+++ banner.js
@@ -0,0 +1,9 @@
+// banner.js
+import chalk from 'chalk';
+
+export function displayBanner() {
+    console.log(chalk.cyan(`
+        AUTHOR : BOYOKE ENCOK
+        ${chalk.yellow('KALEIDO AUTO FARMING')}
+    `));
+}
